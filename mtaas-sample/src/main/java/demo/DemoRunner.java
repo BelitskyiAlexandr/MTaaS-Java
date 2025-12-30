0c1c51ed4a16677bf0d85b32adf7d9d77ca283bf
BelitskyiAlexandr <vilisapett@gmail.com>
2025-12-30 23:36:33 +0200

    subsidiary commit

---
+++ mtaas-sample/src/main/java/demo/DemoRunner.java
@@ -0,0 +1,16 @@
+package demo;
+
+//import demo.order.OrderGenModel;
+//import mtaas.generated.order_total.MetamorphicFunction_order_total;
+
+public class DemoRunner {
+//    public static void main(String[] args) {
+//        OrderGenModel model = new OrderGenModel(3);
+//
+//        MetamorphicFunction_order_total fn = new MetamorphicFunction_order_total();
+//
+//        boolean ok = fn.run(model);
+//
+//        System.out.println("Relation order-total result: " + ok);
+//    }
+}
